fab0b815ccb5ca79486ba638be3fedfca9366c11
Cube <89196797+Cubescubes@users.noreply.github.com>
2025-02-16 11:29:00 +0300

    Add files via upload

--- AgentController.cs
+++ AgentController.cs
@@ -3,7 +3,10 @@ using UnityEngine.AI;
 
 public class AgentController : MonoBehaviour
 {
-public Transform target; // Ссылка на цель
+
+public GameObject MainCamera; // Ссылка на цель
+
+Vector3 Dir = Vector3.zero;
 
 private NavMeshAgent agent;
 
@@ -11,30 +14,31 @@ private float speed;
 
 private float currentspeed;
 
-void Start()
-{
-// Получаем компонент NavMeshAgent
-agent = GetComponent<NavMeshAgent>();
-speed = 3.5f;
+void Start(){
+    agent = GetComponent<NavMeshAgent>();
 }
 
 void Update()
 {
-
+    Dir = Vector3.Normalize(MainCamera.transform.forward);
+    Debug.Log(Dir);
     Vector3 offset = new Vector3();
 // Задаем цель для агента
-    offset = GetBaseInput()* speed * Time.deltaTime;
-    agent.Move(offset);
-    if (currentspeed != agent.speed) {
-        offset= offset * 2;
+    //offset = GetBaseInput()* agent.speed * Time.deltaTime;
+    //Debug.Log(!GetBaseInput().Equals(Vector3.zero));
+    if( !GetBaseInput().Equals(Vector3.zero) ){
+    offset =  GetBaseInput() * agent.speed * Time.deltaTime;
+    
+    //Debug.Log(Dir + GetBaseInput());
+    //cameraInput();
+    //Debug.Log(Input.mousePosition); 
     }
-    Debug.Log(offset); 
-
+    offset =  Dir * agent.speed * Time.deltaTime;
+    agent.Move(offset);
 }
 private Vector3 GetBaseInput(){
 
 Vector3 destination = new Vector3();
-
 
 if (Input.GetKey (KeyCode.W)){ 
             destination += Vector3.forward;              
@@ -50,4 +54,7 @@ if (Input.GetKey (KeyCode.D)){
         } 
 return destination;
 }
+
+
+
 }
--- MoveCamera.cs
+++ MoveCamera.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using System.Collections;
 
 public class MoveCamera : MonoBehaviour
 {
+
+    
+float camSens = 2f;
+
+private Vector3 lastMouse = new Vector3(255, 255, 255);
+    Vector3 Dir = Vector3.zero;
     public Transform player; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,6 +19,13 @@ public class MoveCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Dir = transform.forward;
         this.gameObject.transform.position = player.position;
+        lastMouse = Input.mousePosition - lastMouse ; 
+        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 ); 
+        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0); 
+        transform.eulerAngles = lastMouse; 
+        lastMouse =  Input.mousePosition;
+       // Debug.Log(Dir); 
     }
 }
